f4ef204c625425b0b02803a02b4398a36e5fd827
Seamus Lowry <spl1owry3@gmail.com>
2022-10-23 11:41:56 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+'''Setup for package publish'''
+
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hundred_and_ten",
+    version="0.0.2",
+    author="Seamus Lowry",
+    description="A package to play the game Hundred and Ten",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    # py_modules=["quicksample"],             # Name of the python package
+    # package_dir={'': 'quicksample/src'},     # Directory of the source code of the package
+    # install_requires=[]                     # Install other dependencies if any
+)
