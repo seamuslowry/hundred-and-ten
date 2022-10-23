a22a81a1364ed1ecad671296401a4ef2d70bf173
Seamus Lowry <spl1owry3@gmail.com>
2022-10-23 13:36:53 -0400

    rename object

--- tests/test_game.py
+++ tests/test_game.py
@@ -3,8 +3,8 @@ from unittest import TestCase
 
 from hundred_and_ten.constants import GameStatus
 from hundred_and_ten.game import Game
-from hundred_and_ten.game_players import GamePlayers
 from hundred_and_ten.hundred_and_ten_error import HundredAndTenError
+from hundred_and_ten.players import Players
 
 
 class TestCreateGame(TestCase):
@@ -13,7 +13,7 @@ class TestCreateGame(TestCase):
     def test_default_init(self):
         '''Test init a game with minimal info'''
         organizer = 'organizer'
-        game = Game(GamePlayers(organizer))
+        game = Game(Players(organizer))
 
         self.assertIsNotNone(game.uuid)
         self.assertEqual(game.status, GameStatus.WAITING_FOR_PLAYERS)
@@ -22,7 +22,7 @@ class TestCreateGame(TestCase):
     def test_invite(self):
         '''Test inviting a player to a game'''
         invitee = 'invitee'
-        game = Game(GamePlayers(''))
+        game = Game(Players(''))
 
         self.assertFalse(game.player_data.invitees)
 
@@ -33,7 +33,7 @@ class TestCreateGame(TestCase):
     def test_join(self):
         '''Test inviting a player to a game'''
         invitee = 'invitee'
-        game = Game(GamePlayers('', invitees=['invitee']))
+        game = Game(Players('', invitees=['invitee']))
 
         game.join(invitee)
 
@@ -42,21 +42,21 @@ class TestCreateGame(TestCase):
     def test_join_too_many_players(self):
         '''Test inviting a player to a game'''
         invitee = 'invitee'
-        game = Game(GamePlayers('', players=list(range(4)), invitees=['invitee']))
+        game = Game(Players('', players=list(range(4)), invitees=['invitee']))
 
         self.assertRaises(HundredAndTenError, game.join, invitee)
 
     def test_join_not_invited_to_private(self):
         '''Test inviting a player to a game'''
         invitee = 'invitee'
-        game = Game(GamePlayers(''), accessibility='PRIVATE')
+        game = Game(Players(''), accessibility='PRIVATE')
 
         self.assertRaises(HundredAndTenError, game.join, invitee)
 
     def test_join_not_invited_to_public(self):
         '''Test inviting a player to a game'''
         invitee = 'invitee'
-        game = Game(GamePlayers(''))
+        game = Game(Players(''))
 
         game.join(invitee)
 
