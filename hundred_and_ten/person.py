17b312e7361b4e2b8c43fb2050d8df816452e756
Seamus Lowry <spl1owry3@gmail.com>
2022-10-24 17:51:36 -0400

    types for person

--- hundred_and_ten/person.py
+++ hundred_and_ten/person.py
@@ -1,11 +1,12 @@
 '''Provide a class to keep track person information'''
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class Person:
     '''A class to keep track of person information'''
 
-    def __init__(self, identifier: str, roles=None):
+    def __init__(self, identifier: str, roles: Optional[set[str]] = None):
         self.identifier = identifier
         self.roles = roles or set()
