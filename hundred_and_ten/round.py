766207b633dc4b63697ca66032f8b121064a0fca
Seamus Lowry <spl1owry3@gmail.com>
2022-10-24 18:00:09 -0400

    types for round

--- hundred_and_ten/round.py
+++ hundred_and_ten/round.py
@@ -1,39 +1,42 @@
 '''Represent one round of a game of Hundred and Ten'''
 
 
+from typing import Optional
+
 from hundred_and_ten import people
 from hundred_and_ten.constants import RoundRole, RoundStatus
+from hundred_and_ten.person import Person
 
 
 class Round:
     '''A round in the game of Hundred and Ten'''
 
-    def __init__(self, players=None):
+    def __init__(self, players: Optional[list[Person]] = None):
         self.players = players or []
 
     @property
-    def dealer(self):
+    def dealer(self) -> Optional[Person]:
         """The dealer in this round."""
-        return people.by_role(self.players, RoundRole.DEALER)
+        return next(iter(people.by_role(self.players, RoundRole.DEALER)), None)
 
     @property
-    def bidders(self):
+    def bidders(self) -> list[Person]:
         """The bidders this round, anyone that has submitted a bid."""
         return people.by_role(self.players, RoundRole.BIDDER)
 
     @property
-    def unknowns(self):
+    def unknowns(self) -> list[Person]:
         """Unknowns in this round, anyone that hasn't had a chance to submit a bid."""
         return people.by_role(self.players, RoundRole.UNKNOWN)
 
     @property
-    def active_bidder(self):
+    def active_bidder(self) -> Optional[Person]:
         """The active bidder this round."""
 
         # no active bidder until bidding is implemented
         return None
 
     @property
-    def status(self):
+    def status(self) -> RoundStatus:
         """The status property."""
         return RoundStatus.BIDDING
