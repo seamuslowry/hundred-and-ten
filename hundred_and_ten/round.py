ec6b88f9f01450222c78d112dfc408e8c3fd6506
Seamus Lowry <spl1owry3@gmail.com>
2022-10-24 16:17:52 -0400

    add players to round

--- hundred_and_ten/round.py
+++ hundred_and_ten/round.py
@@ -7,8 +7,8 @@ from hundred_and_ten.constants import RoundStatus
 class Round:
     '''A round in the game of Hundred and Ten'''
 
-    def __init__(self):
-        pass
+    def __init__(self, players=None):
+        self.players = players or []
 
     @property
     def status(self):
