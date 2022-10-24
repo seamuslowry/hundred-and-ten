bedeffca61f239115cb4cdbac82d23fac54e7c5f
Seamus Lowry <spl1owry3@gmail.com>
2022-10-24 18:14:59 -0400

    add type checking to pipeline

--- tests/test_waiting_for_players_game.py
+++ tests/test_waiting_for_players_game.py
@@ -1,7 +1,7 @@
 '''Test behavior of the Game while it is Waiting for Players'''
 from unittest import TestCase
 
-from hundred_and_ten.constants import GameStatus, GameRole
+from hundred_and_ten.constants import GameRole, GameStatus
 from hundred_and_ten.game import Game
 from hundred_and_ten.hundred_and_ten_error import HundredAndTenError
 from hundred_and_ten.person import Person
