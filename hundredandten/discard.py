401f54676b0311105cd08314c3f1b88e5546be6d
Seamus Lowry <spl1owry3@gmail.com>
2022-11-02 17:27:22 -0400

    add discard class

---
+++ hundredandten/discard.py
@@ -0,0 +1,11 @@
+'''Track a trick'''
+from dataclasses import dataclass
+
+from hundredandten.deck import Card
+
+
+@dataclass
+class Discard:
+    '''A class to keep track of one player's discard action'''
+    identifier: str
+    cards: list[Card]
