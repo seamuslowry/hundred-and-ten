9c800a8673102277b12ddf66b67cd84fa06d2d19
Seamus Lowry <spl1owry3@gmail.com>
2022-11-05 10:01:17 -0400

    more useful exports

--- hundredandten/__init__.py
+++ hundredandten/__init__.py
@@ -1,4 +1,4 @@
 '''Init the hundredandten module'''
 from hundredandten.actions import Bid, Discard, Play, SelectTrump, Unpass
-from hundredandten.constants import BidAmount
+from hundredandten.constants import BidAmount, SelectableSuit
 from hundredandten.game import Game as HundredAndTen
